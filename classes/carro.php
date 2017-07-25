add435c966f4c0c77d23c86815e7d4b553511718
EricksonMartinez <erickson.martinez@before.com.br>
2017-07-25 19:41:20 -0400

    DEV-01
    Implements getters and setters, show display objects

--- classes/carro.php
+++ classes/carro.php
@@ -7,17 +7,66 @@ class Carro {
     private $placa;
     private $cor;
 
-    function __construct($id, $fabricante, $modelo, $placa, $cor){
+    function __construct($id=null, $fabricante=null, $modelo=null, $placa=null, $cor=null)
+    {
         $this->id = $id;
         $this->fabricante = $fabricante;
         $this->modelo = $modelo;
         $this->placa = $placa;
         $this->cor = $cor;
+
     }
 
-    //Implementar os métodos Get e Sets das variáveis 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function setFabricante($fabricante)
+    {
+        $this->fabricante = $fabricante;
+    }
+
+    public function setModelo($modelo)
+    {
+        $this->modelo = $modelo;
+    }
+
+    public function setPlaca($placa)
+    {
+        $this->placa = $placa;
+    }
+
+    public function setCor($cor)
+    {
+        $this->cor = $cor;
+    }
+
+
+    public function getFabricante()
+    {
+        return $this->fabricante;
+    }
+
+    public function getModelo()
+    {
+        return $this->modelo;
+    }
+
+    public function getPlaca()
+    {
+        return $this->placa;
+    }
+
+    public function getCor()
+    {
+        return $this->cor;
+    }
 
 }
-
-
 ?>
--- index.php
+++ index.php
@@ -5,10 +5,59 @@ ini_set('display_errors', 1);
 
 require 'classes/carro.php';
 
+$carros[] = new Carro();
 $carros = [];
 $carros[] = new Carro(1,'GM','Gol 1.0','HGG-5014','Prata');
 $carros[] = new Carro(2,'BMW','Série X3','AAA-3097','Vermelha');
 $carros[] = new Carro(3,'Chevrolet','Celta 1.0','BAS-3512','Preta');
 
 
+$carro = new Carro();
+$carro->setId(4);
+$carro->setFabricante('Fiat');
+$carro->setModelo('Palio');
+$carro->setPlaca('HRE-1988');
+$carro->setCor('Branco');
+$carros[] = $carro;
+
 ?>
+
+<html>
+<head>
+<title>Desafio programador</title>
+    <style>
+        table,th,td{
+            border: 1px solid black;
+        }
+    </style>
+</head>
+<body>
+<table>
+
+
+    <tr>
+        <th>Registro</th>
+        <th>Fabricante</th>
+        <th>Modelo</th>
+        <th>Placa</th>
+        <th>Cor</th>
+
+        <?php
+        for ($i =0; $i < 4;$i++)
+        {
+            echo "<tr>";
+            echo "<td>".$carros[$i]->getId()."</td>";
+            echo "<td>".$carros[$i]->getFabricante()."</td>";
+            echo "<td>".$carros[$i]->getModelo()."</td>";
+            echo "<td>".$carros[$i]->getPlaca()."</td>";
+            echo "<td>".$carros[$i]->getCor()."</td>";
+            echo "</tr>";
+
+        }
+        ?>
+
+</table>
+</body>
+</html>
+
+<?php
